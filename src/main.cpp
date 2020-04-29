4046408e69e4ab9ff0a8736fca0e9a5f76bf2d32
Yadi Cao <elliott.yd.cao@outlook.com>
2020-04-28 19:18:02 -0700

    initi commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+
+// #include "Renderer.hpp"
+// #include "Scene.hpp"
+// #include "Triangle.hpp"
+// #include "Sphere.hpp"
+// #include "Vector.hpp"
+// #include "global.hpp"
+// #include <chrono>
+#include "SnowParticle.hpp"
+#include "Sphere.hpp"
+#include <iostream>
+
+// Main function for snow simulation
+int main(int argc, char **argv)
+{
+    std::cout << "hello snow simulation" << std::endl;
+    return 0;
+}
